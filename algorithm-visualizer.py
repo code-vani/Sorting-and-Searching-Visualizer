fb4f3346d060a992441298e978790f7399db4eac
Vanshika Garg <vanshikagarg.20744@gmail.com>
2025-01-23 16:32:46 +0530

    Update algorithm-visualizer.py

--- algorithm-visualizer.py
+++ algorithm-visualizer.py
@@ -27,7 +27,7 @@ st.markdown(
 
     /* Sidebar header styling */
     .css-1d391kg h1 {
-        color: black; /* Set the header text color to white */
+        color: black !important; /* Set the header text color */
     }
 
     /* Heading styling - ensure the title stays white */
